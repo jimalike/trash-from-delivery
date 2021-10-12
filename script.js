98c0495ecd1ff7ddec98513240d4ffba59cec272
champwow <sukawit1909@gmail.com>
2021-10-12 13:54:58 +0700

    demo2

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ document.body.onscroll = function() {
     let scrollY = window.scrollY;
     let viewHeight = window.innerHeight;
     document.querySelector("#varY").innerText = scrollY;
+    var condi1 = document.getElementById("condi-1");
+    var condi2 = document.getElementById("condi-2");
+    var condi3 = document.getElementById("condi-3");
+    var condi4 = document.getElementById("condi-4");
 
-    if (scrollY >= 0 && scrollY <= 240) {
+    var condi1Top = condi1.offsetTop;
+    var condi2Top = condi2.offsetTop;
+    var condi3Top = condi3.offsetTop;
+    var condi4Top = condi4.offsetTop;
+    if (scrollY + window.innerHeight - (condi1.offsetHeight) >= condi1Top && scrollY < condi1Top) {
         document.querySelector(".big-receipt").dataset.scene = 1
-    } else if ((scrollY >= 240 && scrollY <= 480)) {
+    } else if (scrollY + window.innerHeight - (condi2.offsetHeight) >= condi2Top && scrollY < condi2Top) {
         document.querySelector(".big-receipt").dataset.scene = 2
-    } else if ((scrollY >= 480 && scrollY <= 720)) {
+    } else if (scrollY + window.innerHeight - (condi3.offsetHeight) >= condi3Top && scrollY < condi3Top) {
         document.querySelector(".big-receipt").dataset.scene = 3
-    } else if ((scrollY >= 720 && scrollY <= 940)) {
+    } else if (scrollY + window.innerHeight - (condi4.offsetHeight)) {
         document.querySelector(".big-receipt").dataset.scene = 4
     }
 }
